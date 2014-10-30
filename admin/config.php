d6e4f390a22b1f159065bd6727a1bd078dc28921
hankerstudio <hankerstudio@hotmail.com>
2014-10-30 23:12:26 +0800

    con

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://hkshop.sturgeon.mopaas.com/admin/');
+define('HTTP_CATALOG', 'http://hkshop.sturgeon.mopaas.com/');
+define('HTTP_IMAGE', 'http://hkshop.sturgeon.mopaas.com/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://hkshop.sturgeon.mopaas.com/admin/');
+define('HTTPS_IMAGE', 'http://hkshop.sturgeon.mopaas.com/image/');
+
+// DIR
+define('DIR_APPLICATION', '/var/vcap.local/dea/apps/hkshop-0-fddbb4d6f12f716633f3f0d26af162b0/app/admin/');
+define('DIR_SYSTEM', '/var/vcap.local/dea/apps/hkshop-0-fddbb4d6f12f716633f3f0d26af162b0/app/system/');
+define('DIR_DATABASE', '/var/vcap.local/dea/apps/hkshop-0-fddbb4d6f12f716633f3f0d26af162b0/app/system/database/');
+define('DIR_LANGUAGE', '/var/vcap.local/dea/apps/hkshop-0-fddbb4d6f12f716633f3f0d26af162b0/app/admin/language/');
+define('DIR_TEMPLATE', '/var/vcap.local/dea/apps/hkshop-0-fddbb4d6f12f716633f3f0d26af162b0/app/admin/view/template/');
+define('DIR_CONFIG', '/var/vcap.local/dea/apps/hkshop-0-fddbb4d6f12f716633f3f0d26af162b0/app/system/config/');
+define('DIR_IMAGE', '/var/vcap.local/dea/apps/hkshop-0-fddbb4d6f12f716633f3f0d26af162b0/app/image/');
+define('DIR_CACHE', '/var/vcap.local/dea/apps/hkshop-0-fddbb4d6f12f716633f3f0d26af162b0/app/system/cache/');
+define('DIR_DOWNLOAD', '/var/vcap.local/dea/apps/hkshop-0-fddbb4d6f12f716633f3f0d26af162b0/app/download/');
+define('DIR_LOGS', '/var/vcap.local/dea/apps/hkshop-0-fddbb4d6f12f716633f3f0d26af162b0/app/system/logs/');
+define('DIR_CATALOG', '/var/vcap.local/dea/apps/hkshop-0-fddbb4d6f12f716633f3f0d26af162b0/app/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', '10.4.12.46');
+define('DB_USERNAME', 'u14oUoae1gFL0');
+define('DB_PASSWORD', 'p5Xxk8wkJ3u2T');
+define('DB_DATABASE', 'd97fdd23635dc4d0297f698a91a03380f');
+define('DB_PREFIX', '');
+?>
